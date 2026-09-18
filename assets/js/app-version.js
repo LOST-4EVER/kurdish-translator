dcf943b7790d8e820dde3c863287b9afb64a12a8
LOST-4EVER <139042138+LOST-4EVER@users.noreply.github.com>
2026-09-18 13:21:13 +0300

    chore: bump application version to v161

--- assets/js/app-version.js
+++ assets/js/app-version.js
@@ -3,7 +3,7 @@
  * Exposes AppVersion as a global module.
  */
 const AppVersion = (() => {
-  const APP_VERSION = 'v151';
+  const APP_VERSION = 'v161';
   let isRefreshing = false;
   let hasShownUpdateNotice = false;
   let hasUpdateAvailable = false;
